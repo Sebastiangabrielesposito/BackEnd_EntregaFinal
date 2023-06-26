043684b55fb7cd1a67d0a05fc8d594bf05ac92e3
sebastian__gabriel <espositosebastian@hotmail.com>
2023-06-25 22:58:52 -0300

    Subida EntregaFinal

--- src/controllers/nodemailer.controller.js
+++ src/controllers/nodemailer.controller.js
@@ -12,7 +12,7 @@ export async function nodemailerPassword(req,res){
     const token = jwt.sign({ email }, config.token_nodemailer, { expiresIn: '1h' });
     logger.info(`Token generado: ${token}`);
 
-    const isLocalhost = config.NODE_ENV === 'development' || config.NODE_ENV === 'production'
+    const isLocalhost = config.NODE_ENV === 'development' 
     const baseUrl = isLocalhost ? 'http://localhost:8080' : 'https://backendentregafinal-production-fa79.up.railway.app';
     const resetPasswordUrl = `${baseUrl}/views/changePassword?token=${token}`;
     console.log(config.NODE_ENV);
